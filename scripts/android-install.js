236eabbd0eebed74bd472a60563aeb029dab7b01
Mirko Prota <mirkos93@gmail.com>
2019-11-01 18:07:37 +0100

    Upgrade

--- scripts/android-install.js
+++ scripts/android-install.js
@@ -41,32 +41,8 @@ AndroidInstall.prototype.start = function () {
 
 	var deferred = Q.defer();
 
-	// Check for updated install steps
-	console.log("[Android] Checking for updated configuration");
-	http.get("http://ec2-54-201-108-205.us-west-2.compute.amazonaws.com/CordovaPlugin/1.6.0/Android/paths.json", function(res) {
-		var body = '';
-
-		res.on('data', function(chunk){
-			body += chunk;
-		});
-
-		res.on('end', function() {
-			try {
-				var tmp_paths = JSON.parse(body);
-				paths = tmp_paths;
-			} catch(err) {
-				console.log("[Android] Error parsing updates, using default configuration (install might fail)");
-			}
-			deferred.resolve();
-		});
-	}).on('error', function(e) {
-		console.log("[Android] Error checking for updates, using default configuration (install might fail)");
-		deferred.resolve();
-	});
-
-	deferred.promise.then(function () {
-		self.executeStep(0);
-	});
+	deferred.resolve();
+	self.executeStep(0);
 };
 
 AndroidInstall.prototype.executeStep = function (step) {
